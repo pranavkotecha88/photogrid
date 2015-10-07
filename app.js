09a4171962be029679b5b36b405d9e5bcda3f927
Pranav Kotecha <pranavkotecha88@gmail.com>
2015-10-07 16:51:04 -0700

    Setting up Formidable, lecture 45

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require('express'),
     path = require('path'),
     config = require('./config/config.js')
-    knox = require('knox');
+    knox = require('knox'),
+    fs = require('fs'),
+    os = require('os'),
+    formidable = require('formidable');
 
 var app = express();
 app.set('views', path.join(__dirname, 'views'));
@@ -15,10 +18,10 @@ app.set('host', config.host);
 var knoxClient = knox.createClient({
   key: config.S3AccessKey,
   secret: config.S3Secret,
-  bucket: config.S3Bucket 
+  bucket: config.S3Bucket
 });
 
-require('./routes/routes.js')(express, app);
+require('./routes/routes.js')(express, app, formidable, fs, os);
 
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,41 @@
-module.exports = function(express, app){
+module.exports = function(express, app, formidable, fs, os){
   var router = express.Router();
 
   router.get('/', function(req, res, next){
     res.render('index', {host:app.get('host')});
   });
+
+  router.post('/upload', function(req, res, next){
+
+    function generateFileName(fileName) {
+      var ext_regex = /(?:\.([^.]+))?$/;
+      var ext = ext_regex.exec(fileName)[1];
+      var date = new Date().getTime();
+      var charBank = "abcdefghijklmnopqrstuvwxyz";
+      var fstring = '';
+      for (var i=0; i<15; i++) {
+        fstring += charBank[parseInt(Math.random()*26)];
+      }
+      return (fstring += date + '.' + ext);
+    }
+
+    var tmpFile, nFile, fName;
+    var newForm = new formidable.IncomingForm();
+        newForm.keepExtensions = true;
+        newForm.parse(req, function(err, fields, files){
+          tmpFile = files.upload.path;
+          fName = generateFileName(files.upload.name);
+          nFile = os.tmpDir() + '/' + fName;
+          res.writeHead(200, {'Content-type':'text/plain'});
+          res.end();
+        })
+
+        newForm.on('end', function(){
+          fs.rename(tmpFile, nFile, function(){
+
+          })
+        })
+  })
 
   app.use('/', router);
 
