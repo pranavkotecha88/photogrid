7dfe7d7a8ba12b93dd40568e83152ccb26a5ed4b
Pranav Kotecha <pranavkotecha88@gmail.com>
2015-10-07 16:24:39 -0700

    Setting up Formidable for receiving files and upload handler, Lecture 44

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ module.exports = function(express, app){
   var router = express.Router();
 
   router.get('/', function(req, res, next){
-    res.render('index', {})
+    res.render('index', {host:app.get('host')});
   });
 
   app.use('/', router);
